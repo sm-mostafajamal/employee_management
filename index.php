8a59fca26d189eaa77dfbdd53843a5b308d0cf76
sm.mostafajamal <sm.mostafajamal@gmail.com>
2023-07-29 06:56:37 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employee Management</title>
+</head>
+<body>
+    <div class="top">
+        <input type="text" name="search" id="search" placeholder="Search..." />
+        <button>Add Employee</button>
+    </div>
+</body>
+</html>
